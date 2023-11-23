aab94732e60a56987ad2ff5e215ecf40d9794395
DaddyDarky <93772288+DaddyDarky@users.noreply.github.com>
2023-11-23 14:36:13 +0100

    primer_commit

---
+++ Ejercicio1LuisManuel.js
@@ -0,0 +1,63 @@
+/** Ejercicios JavaScript 
+ *  Autor: Luis Manuel
+ *  Edicion: SuiL LeunaM
+*/ 
+//1. Haz los siguientes ejercicios simples de cadenas:
+    
+//a. Crea una cadena multilínea con comillas dobles.
+var luisMultiLinea = 'Modernia ' + ' Nihilister ' + ' Red Hood';
+console.log(luisMultiLinea);
+
+//b. Añade a la cadena algún retorno de carro y tabuladores con el símbolo de escape
+var luisMultiLinea2 = 'Modernia\n\tNihilister\n\t\tRed Hood';
+console.log(luisMultiLinea2);
+
+//c. Añade a la cadena el carácter \
+var luisMultiLinea3 =  "\\Modernia \n\t \\Nihilister \n\t\t \\Red Hood";
+console.log(luisMultiLinea3)
+
+//d. Concatena otra cadena con el operador +
+var concatenarLinea = 'Illo te digo que '
+var concatenarAenil = 'Vaya culo tengo '
+console.log(concatenarLinea+concatenarAenil)
+
+//e. Concatena cadenas usando una template string, muestra el valor de varias
+    //variables en el template string
+
+//f. Separa un texto que tenga varias frases en un array que tenga cada frase por
+    //separado.
+
+//g. Convierte un texto dado a minúsculas.
+
+//h. Convierte un texto dado a mayúsculas.
+
+//i. Recorre con un bucle el texto caracter a caracter imprimiéndolo.
+
+//j. Busca una subcadena en un texto.
+
+//k. Extrae en una variable la subcadena desde la posición 3 hasta el final del
+    //texto.
+
+//l. Extrae en una variable la subcadena desde la posición 3 hasta la primera
+    //ocurrencia de una palabra en el texto.
+
+//m. Reemplaza todos los espacios del texto por un -
+
+//n. Elimina los espacios antes y después del texto.
+
+//o. Crea una cadena que no tenga ningún espacio partiendo de otra dada.
+
+//p. Crea una función que invierta una cadena de texto.
+
+//q. Usa una expresión regular para comprobar que la cadena tiene números.
+
+//r. Usa una expresión regular para comprobar que la cadena termina en punto.
+
+//s. Usa una expresión regular para comprobar que la cadena comienza por una
+    //mayúscula.
+
+//t. Usa una expresión regular para comprobar si la cadena contiene un teléfono
+    //con código internacional.
+
+  //u. Reemplaza cualquier ocurrencia de un + seguido de números por la cadena
+    //SECRETO
